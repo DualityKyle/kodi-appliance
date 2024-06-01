e9007ad661a1792fd37770946687c5d87e921112
Kyle McGrath <26319313+DualityKyle@users.noreply.github.com>
2024-06-01 00:10:12 -0300

    bugfix

--- kodi-install.sh
+++ kodi-install.sh
@@ -668,6 +668,8 @@ postinstall_setup () {
         echo 'GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT rd.systemd.show_status=auto rd.udev.log_level=3 fbcon=nodefer"' > /mnt/etc/default/grub.d/99-silent-boot.cfg
         arch-chroot /mnt grub-install --target=i386-pc "$DISK" &> /dev/null
 	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg &> /dev/null
+        # Suppress "Loading Linux" messages from grub
+	sed -i 's/echo/#echo/g' /mnt/boot/grub/grub.cfg
     fi
 
 }
